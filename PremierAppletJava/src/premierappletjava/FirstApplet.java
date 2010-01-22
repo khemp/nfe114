99a2d32b9387a5c25de94a2aaa19e9b3dc160043
paulk <paul.khem@gmail.com>
2010-01-22 19:30:45 +0100

    premier applet java (marche pas)

---
+++ PremierAppletJava/src/premierappletjava/FirstApplet.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package premierappletjava;
+
+//import java.awt.*;
+import java.applet.Applet;
+import java.awt.Label;
+
+
+public class FirstApplet extends java.applet.Applet {
+   public void init (){
+        add(new Label("Hello World"));
+   }
+}
